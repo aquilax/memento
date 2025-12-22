6816c305588dcea07a482fe5d806f3b7dc3160c4
aquilax <aquilax@gmail.com>
2025-12-22 17:36:18 +0100

    Description

---
+++ scripts/miranda/import-miranda-sqlite3-dump.py
@@ -0,0 +1,6 @@
+# A scipt to import contacts and messages from sqlite3 dump geenrated by the 
+# https://github.com/ostankin/mirandat3 script
+#
+# Usage:
+# python3 scripts/miranda/import-miranda-sqlite3-dump.py contacts export.sqlite3 > contacts.json
+# python3 scripts/miranda/import-miranda-sqlite3-dump.py messages export.sqlite3 > messages.json
